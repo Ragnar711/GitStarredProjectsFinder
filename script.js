7e9cb0ea989ff5f8146d9c42ffc8b77f1cd0c058
Mohamed Aziz Bechaib <azizbechaib711@gmail.com>
2023-06-21 16:57:18 +0100

    save

---
+++ script.js
@@ -0,0 +1,29 @@
+$(document).ready(function () {
+    $("form").submit(function (event) {
+        event.preventDefault();
+        var startDate = $("#start-date").val();
+        var endDate = $("#end-date").val();
+        var url =
+            "https://api.github.com/search/repositories?q=created:" +
+            startDate +
+            ".." +
+            endDate +
+            "&sort=stars&order=desc";
+        $.get(url, function (response) {
+            var projects = response.items;
+            var results = $("#results");
+            results.empty();
+            projects.forEach(function (project) {
+                var card = $("<div>").addClass("card");
+                var title = $("<h2>").text(project.name);
+                var description = $("<p>").text(project.description);
+                var stars = $("<p>").text("Stars: " + project.stargazers_count);
+                var link = $("<a>")
+                    .attr("href", project.html_url)
+                    .text("View on GitHub");
+                card.append(title, description, stars, link);
+                results.append(card);
+            });
+        });
+    });
+});
